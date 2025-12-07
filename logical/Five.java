9c3f54bb048731626e7b4f5b6b17960f6203122a
nandkishorhiwale321 <nandkishorhiwale321@gmail.com>
2025-12-07 20:24:02 +0530

    logical progarm'

---
+++ logical/Five.java
@@ -0,0 +1,21 @@
+package logical;
+
+public class Five {
+	public static void main (String [] args)
+	{
+		/*
+		 * program for even 1 to 100 numbers which is greater than 50.
+		 */
+		
+		for(int i=1; i<=100; i++) {
+			
+			if(i%2==0) {
+				
+				if(i>=50)
+					
+				System.out.println("even number"+i);
+			}
+		}
+		
+	}
+	}
