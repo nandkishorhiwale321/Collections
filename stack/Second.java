f8e9d0516fefad40d86677d478dae746831266a4
nandkishorhiwale321 <nandkishorhiwale321@gmail.com>
2025-12-12 22:42:25 +0530

    stack

---
+++ stack/Second.java
@@ -0,0 +1,13 @@
+package stack;
+import java.util.*;
+
+public class Second {
+	public static void main (String [] args) {
+	HashSet<Integer> ar = new HashSet<>();
+	ar.add(12);
+	ar.add(13);
+	ar.add(12);
+	System.out.println(ar);
+	}	
+
+}
