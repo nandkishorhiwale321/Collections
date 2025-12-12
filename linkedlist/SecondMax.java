767d376bb373f4a9624a4d126dd7e77eef34abea
nandkishorhiwale321 <nandkishorhiwale321@gmail.com>
2025-12-12 22:46:03 +0530

    find max element

---
+++ linkedlist/SecondMax.java
@@ -0,0 +1,25 @@
+package linkedlist;
+import java.util.ArrayList;
+
+import java.util.Collections;
+
+public class SecondMax {
+	public static void main (String [] args) {
+		
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		
+		Collections.addAll(a, 12,32,55,5,64,5,0 ,99,154,22,10,54);
+		
+		
+	int max =a.get(0) ;
+	for(int i=0; i<a.size();i++) {
+		if(max<a.get(i)) {
+			max=a.get(i);
+			a.getLast();
+			
+		}	
+		}
+	System.out.println("max);
+	}
+
+}
