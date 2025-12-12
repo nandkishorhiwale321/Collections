08f426efe618d0dfcc54bdc792610de69e24e4e2
nandkishorhiwale321 <nandkishorhiwale321@gmail.com>
2025-12-12 22:44:01 +0530

    Arraylist pro.

---
+++ arraylist/Forth.java
@@ -0,0 +1,25 @@
+package arraylist;
+
+public class Forth {
+public static void main (String [] args) {
+	/*
+	 * calculate how many char.. of given string
+	 * 	 
+	 *
+	 */
+
+	String s = "NandkishorGaneshHiwale";
+	
+	int counter =0;
+	
+	for(int i=0; i<s.length();i++) {
+		
+			++counter ;
+		}
+		
+	System.out.println(counter);
+		
+	}
+
+	
+}
