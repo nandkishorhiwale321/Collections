a13bcc6e1feafcbee96d738f721731b3464f058f
nandkishorhiwale321 <nandkishorhiwale321@gmail.com>
2025-12-16 21:46:14 +0530

    remove duplicates

---
+++ linkedlist/Five.java
@@ -0,0 +1,22 @@
+package linkedlist;
+import java.util.*;
+
+   public class Five {
+	   
+	  public static void main (String [] args) {
+		
+		List<Integer> list = new ArrayList<Integer>();
+		
+		Collections.addAll(list, null,01,01,33,3,05,3,10,2,13,21,25,null);
+		
+		
+		Set<Integer> set = new HashSet<Integer>();
+		
+		set.addAll(list);
+		
+		System.out.println("Origanal list :"+list);
+		System.out.println("Removed Elements list :"+set);
+	}
+	
+
+}
