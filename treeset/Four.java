122d2ee983ae2c6adeb971847961e44161fa5215
nandkishorhiwale321 <nandkishorhiwale321@gmail.com>
2025-12-24 08:22:17 +0530

    TreeSet

--- treeset/Four.java
+++ treeset/Four.java
@@ -2,21 +2,40 @@ package treeset;
 import java.util.TreeSet;
 
 public class Four {
-
+	public static void main (String [] args) {
+		
+		TreeSet <student> tree= new TreeSet<student>();
+		
+		student s = new student("Madhav",101,100);
+		student s2 =new student ("gerish",102,90);
+		
+		tree.add(s);
+		tree.add(s2);
+		
+		System.out.println(tree);
+	}
 }
-class Student {
+class student implements Comparable <student>{
 	
 	int rollnumber;
 	String name;
 	int marks;
 	
-	public Student(String name,int rollnumber,int marks) {
+	public student(String name,int rollnumber,int marks) {
 		this.name=name;
 		this.marks=marks;
 		this.rollnumber=rollnumber;
 	}
 	
 	public String toString() {
-		return this.name+this.marks+this.rollnumber;
+		return this.name+" "+this.marks+" "+this.rollnumber;
+	}
+
+	
+
+	@Override
+	public int compareTo(student o) {
+		// TODO Auto-generated method stub
+		return Integer.compare(o.marks,this.marks);
 	}
 }
